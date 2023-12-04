0d3b16b4482e6c9bccb913a21cdec9074c7d13d6
peterpan1905 <mrpiercewalker@gmail.com>
2023-12-04 11:19:02 -0700

    Added views directory

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Author: Pierce Walker, Nathan Moore, Traeden Overly, Patrick Petty
 const express = require("express");
 
-let app = EXPRESS();
+let app = express();
 
 let path = require("path");  
 
@@ -10,18 +10,18 @@ const port = 3000;
 app.use(express.urlencoded({extended: true})); // gets the .value of tags in a form
 
 app.get("/", (req, res) => {
-    res.send("This is my first node express application");
+    res.send("");
 });
 
-app.get("/help", (req,res) => {
-    res.send("Please contact tech support");
+app.post("/login", (req,res) => {
+    res.send("");
 });
 
-app.get("/testme", (req, res) => {
+app.post("/socialMedia", (req, res) => {
     res.sendFile(path.join(__dirname + '/test.html'));
 });
 
-app.post("/storeIt", (req, res) => {
+app.post("/report", (req, res) => {
     res.send(req.body.empFirst);
 });
 
