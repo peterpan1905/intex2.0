0f0f7134d3043493e6a29d3431c9606245aaa378
nathanmoore101 <103144908+nathanmoore101@users.noreply.github.com>
2023-12-04 11:16:03 -0700

    added database to index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // Author: Pierce Walker, Nathan Moore, Traeden Overly, Patrick Petty
 const express = require("express");
 
-let app = EXPRESS();
+let app = express();
 
 let path = require("path");  
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true})); // gets the .value of tags in a form
+
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host : process.env.RDS_HOSTNAME || "localhost",
+        user : process.env.RDS_USERNAME || "postgres",
+        password : process.env.RDS_PASSWORD || "buddy",
+        database : process.env.RDS_DB_NAME || "music",
+        port : process.env.RDS_PORT || 5433,
+        ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
+    }
+}); 
 
 app.get("/", (req, res) => {
     res.send("This is my first node express application");
